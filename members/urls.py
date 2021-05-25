3fd304eaefe261f02e4fe83bc94f548c0b1ebf6e
YOUR NAME <YOUR@EMAIL.com>
2021-05-25 12:24:16 +0200

    add edit-profile page

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserUpdateProfileView
 
 urlpatterns = [
     path("u-register", UserRegisterView.as_view(), name="register"),
+    path("edit_profile/", UserUpdateProfileView.as_view(), name="edit_profile"),
+
 ]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+from django.views import generic
 from members.forms import SignUpForm
 from django.shortcuts import render
 from django.views.generic import (
@@ -9,10 +10,20 @@ from django.views.generic import (
 )
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 
 # Create your views here.
 class UserRegisterView(CreateView):
     form_class = SignUpForm
     template_name = "registration/register.html"
     success_url = reverse_lazy("login")
+
+
+class UserUpdateProfileView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = "registration/edit_profile.html"
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+        return self.request.user
