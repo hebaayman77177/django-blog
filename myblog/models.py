d9644707bcf2e749b87ff4b11bd76d0f67e6c2fb
YOUR NAME <YOUR@EMAIL.com>
2021-05-26 18:06:59 +0200

    add profile pic and social media links

--- myblog/models.py
+++ myblog/models.py
@@ -36,6 +36,19 @@ class Post(models.Model):
 class Profile(models.Model):
     user= models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio= models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(max_length=255, null=True, blank=True)
+    fb_url = models.CharField(max_length=255, null=True, blank=True)
+    instgram_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
+    pintrest_url = models.CharField(max_length=255, null=True, blank=True)
+
+
+
+
+
+
+
 
 
     def __str__(self):
